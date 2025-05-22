73c4092433a8b1f7ac45755a8bfae2358dbc5b78
dunamix <techwithdunamix@gmail.com>
2025-05-22 17:35:18 +0100

    Update metadata description and add icons for Eduace

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,13 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Eduace',
-  description: 'Created by dunamx',
+  description: 'Eduace is a learning management system that empowers educators to create, manage, and deliver engaging online courses.',
   generator: '??',
+  icons: {
+    icon: '/images/eduace-logo.png',
+    shortcut: '/images/eduace-logo.png',
+    apple: '/images/eduace-logo.png',
+  },
 }
 
 export default function RootLayout({
